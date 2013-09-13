fa1234fa07badb723fed6b3a1e7668c9052d8f6e
Shaun Thompson <shaunthompson@fullsail.edu>
2013-09-13 15:43:15 -0400

    Have totally forgot to commit anything worked on this week!!! :( all changes are on master git

---
+++ Application/platforms/AVF-Android/android/assets/www/js/main.js
@@ -0,0 +1,21 @@
+$(document).on('pageinit', function(){
+	
+
+	
+	// JSON ajax parse from Instagram API
+	$('#json').on("click", function(){
+		
+		$('#list ul').remove();
+		
+		var tag = "kittens"; //this could also be based on a form field
+		var url = "https://api.instagram.com/v1/media/popular?client_id=67cce2a91aff4039b40cce1214bb49bd";
+		$.getJSON('xhr/data.json', function(data) {
+        	for (var i in data.entries) {
+        		$('#list').append('<ul>' + data.entries[i].name + '</ul>');
+        		$('#list ul').append('<li>' + data.entries[i].date + '</li>');
+        		$('#list ul').append('<li>' + data.entries[i].calories + '</li>');    
+        	}
+  		});
+	
+	
+});
